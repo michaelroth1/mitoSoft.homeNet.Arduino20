3a8d76d39505a8274fef6ae51a9e36ad16b7715c
Michael <michael.mr.roth@live.com>
2025-01-30 23:03:27 +0100

    *[BUGFIX] fix Arduino sketches

--- example/SimpleGPIOUsing/SimpleGPIOUsing.ino
+++ example/SimpleGPIOUsing/SimpleGPIOUsing.ino
@@ -1,15 +1,17 @@
 /*
  Name:		SimpleGPIOUsing.ino
  Created:	1/22/2020 11:47:24 PM
- Author:	M. Roth
+ Author:	Bob
 */
 
 #include <MitoSoft.h>
 
 DebouncingInput shutter1Pos(23, INPUT_PULLUP, 50);
-DebouncingInput shutter1DownTaster(24, INPUT_PULLUP, 50);
-DebouncingInput shutter1UpTaster(25, INPUT_PULLUP, 50);
-ShutterController shutter1(26, 27, 20000, 2000);
+DebouncingInput shutter1DownButton(24, INPUT_PULLUP, 50);
+DebouncingInput shutter1UpButton(25, INPUT_PULLUP, 50);
+ShutterController shutter1(20000, 2000);
+InvertableOutput shutter1Up(44, STANDARD); //INVERTED
+InvertableOutput shutter1Down(45, STANDARD); //INVERTED
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -24,10 +26,10 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 
-	if (shutter1DownTaster.risingEdge()) {
+	if (shutter1DownButton.risingEdge()) {
 		shutter1.runDown();
 	}
-	else if (shutter1UpTaster.risingEdge()) {
+	else if (shutter1UpButton.risingEdge()) {
 		shutter1.runUp();
 	}
 	else if (shutter1Pos.risingEdge()) {
@@ -39,9 +41,19 @@ void loop() {
 
 	if (shutter1.started()) {
 		Serial.println("Started Direction: " + shutter1.getDirectionAsText());
+    if (1 == shutter1.getDirection()) { //DOWN
+      shutter1Up.setOff();
+      shutter1Down.setOn();
+    }
+    else if (2 == shutter1.getDirection()) { //UP
+      shutter1Up.setOn();
+      shutter1Down.setOff();
+    }
 	}
 	else if (shutter1.stopped()) {
 		Serial.println("Stopped Pos: " + String(shutter1.getPosition()) + "; Fin-Pos: " + String(shutter1.getFinPosition()));
+    shutter1Up.setOn();
+    shutter1Down.setOn();
 	}
 	
 	//looping
--- example/SimpleMqttUsing/SimpleMqttUsing.ino
+++ example/SimpleMqttUsing/SimpleMqttUsing.ino
@@ -1,7 +1,11 @@
 /*
  Name:		SimpleMqttUsing.ino
  Created:	1/21/2020 8:49:25 PM
- Author:	M. Roth
+ Author:	Mary
+ Remarks: To test, you can use the command line tool mosquitto_pub shipped with mosquitto 
+ or the mosquitto-clients package to send MQTT messages. 
+ This allows you to operate your cover manually:
+ mosquitto_pub -h 127.0.0.1 -t input/homeAssistant/command/set -m "SHOW"
 */
 
 #include <SPI.h>
@@ -13,8 +17,9 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress broker(192, 168, 2, 100);
 
+EthernetHelper ethHelper(mac);
+
 EthernetClient ethClient;
-EthernetHelper ethHelper(mac, ethClient, broker);
 
 PubSubClient mqttClient(broker, 1883, ethClient);
 PubSubHelper mqttHelper(mqttClient);
@@ -27,27 +32,36 @@ void setup() {
 
     ethHelper.dhcpSetup();
 
-    mqttHelper.connect("Room1Controller", "Room1Controller");
+    mqttHelper.init("ClientId");
 
     Serial.println("start loop");
 }
 
 void loop() {
 
-    String t = mqttHelper.getSubtopic();
-    String m = mqttHelper.getMessage();
+  String t = "";
+  String m = "";
 
-    if (m != "") {
-        Serial.println("Message received in MAIN LOOP: " + m);
-    }
+  if (mqttHelper.onMessageReceived()){
+    t = mqttHelper.getLastTopic();
+    m = mqttHelper.getLastMessage();
+  }    
 
-    // publish a message roughly every second.
-    if (millis() - lastMillis > 1000) {
-        lastMillis = millis();
-        mqttHelper.publish("Output2", "High");
-    }
+  if (m != "") {
+    Serial.println("Message received in MAIN LOOP: " + m);
+  }
 
-    mqttHelper.loop();
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    mqttHelper.publish("Output2", "High");
+  }
 
-    delay(50);
+  if(mqttHelper.onConnected()){
+    mqttHelper.subscribe("input/+/command/#");
+  }
+  
+  ethHelper.loop();
+  mqttHelper.loop();
+  delay(50);
 }
